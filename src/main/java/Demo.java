aa50e446f200636674062afe4d5b170ae0c6263f
Oleg Dudar <oleg_dudar@epam.com>
2016-04-20 17:56:15 +0300

    Added screenshot capture and capabilities for https web sites

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,19 +1,49 @@
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.Augmenter;
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class Demo {
 
     public static void main(String[] args){
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setJavascriptEnabled(true);
+        caps.setCapability("takesScreenshot", true);
         caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "C:\\phantomjs\\bin\\phantomjs.exe");
+        caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, new String[] {"--ssl-protocol=tlsv1"});
+        caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, new String[] {"--web-security=false"});
+        caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, new String[] {"--ignore-ssl-errors=true"});
+//        caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, new String[] {"--webdriver=true"});
+
+        int count = 1;
+        long iStart = System.currentTimeMillis();
 
         WebDriver driver = new PhantomJSDriver(caps);
-        driver.get("http://en.wikipedia.org");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("https://www.eff.org/https-everywhere");
         System.out.println("Page title" + driver.getTitle());
+        driver = new Augmenter().augment(driver);
+        File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        System.out.println("File:" + srcFile);
+        try {
+            FileUtils.copyFile(srcFile, new File("c:\\tmp\\screenshot_" + (count++) + ".png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Single Page Time:" + (System.currentTimeMillis() - iStart));
+        driver.quit();
 
     }
 }
